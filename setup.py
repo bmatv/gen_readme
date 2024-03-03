774bb8f810b1584d37d132b19d077b00c3a753d3
bogdanmatviichuk <hammermolotok@gmail.com>
2024-03-03 16:49:42 +1100

    fixed setup.py and updated .gitignore

--- setup.py
+++ setup.py
@@ -12,25 +12,13 @@ setuptools.setup(
     description="python utility for interactive README file generation",
     author="BM",
     author_email="hammermolotok@gmail.com",
-    # package_data={"gnssanalysis": ["py.typed"]},
-    packages=setuptools.find_packages('src'),
-    package_dir={'': 'src'},
+    packages=setuptools.find_packages(),
     install_requires=[
-        # "os",
-        # "sys",
-        # "re",
+        # required packages with versions go here
     ],
     entry_points={
         "console_scripts": [
             "gen_readme = src.makeReadMe:gen_readme",
-            # "diffutil = gnssanalysis:gn_utils.diffutil",
-            # "snxmap = gnssanalysis:gn_utils.snxmap",
-            # "sp3merge = gnssanalysis:gn_utils.sp3merge",
-            # "log2snx = gnssanalysis:gn_utils.log2snx",
-            # "trace2mongo = gnssanalysis:gn_utils.trace2mongo",
-            # "gnss-filename = gnssanalysis.filenames:determine_file_name_main",
-            # "orbq = gnssanalysis:gn_utils.orbq",
-            # "clkq = gnssanalysis:gn_utils.clkq",
         ]
     },
     long_description=readme_text,  # Provide entire contents of README to long_description
